7e1df2134c085faeed0133774abd02b4b475120f
Artem <artem.aisin@yandex.ru>
2023-11-10 00:54:44 +0300

    feat: cards are displayed

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function renderCard(card, cardDelete) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__title').textContent = card.name;
+
+  deleteButton.addEventListener('click', (evt) => {
+    cardDelete(evt);
+  });
+
+  cardContainer.append(cardElement);
+}
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((card) => {
+  renderCard(card, cardDelete);
+});
